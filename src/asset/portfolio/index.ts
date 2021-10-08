a02eeabfa6b960399d1e38e27f163ff546313813
AhnyoungJun <toinin@naver.com>
2021-10-08 11:49:27 +0900

    [ADD] frontend - svg Image

---
+++ src/asset/portfolio/index.ts
@@ -0,0 +1,7 @@
+export { default as typescript_portfolio } from './typescript.svg';
+export { default as javascript_portfolio } from './javascript.svg';
+export { default as react_portfolio } from './react.svg';
+export { default as springboot_portfolio } from './springboot.svg';
+export { default as nodejs_portfolio } from './nodejs.svg';
+export { default as java_portfolio } from './java.svg';
+export { default as docker_portfolio } from './docker.svg';
